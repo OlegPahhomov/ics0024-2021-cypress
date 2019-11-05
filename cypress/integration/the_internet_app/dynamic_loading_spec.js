1805c827db3daca3d5bac4466ecc4893ff76fed5
German Mumma <german.mumma@ttu.ee>
2019-11-05 17:15:44 +0200

    add dynamic loading test

---
+++ cypress/integration/the_internet_app/dynamic_loading_spec.js
@@ -0,0 +1,14 @@
+describe("Dynamic loading page", function () {
+    before(function () {
+        cy.visit("/dynamic_loading")
+    });
+
+    it('should display Hello World', function () {
+        cy.get("[href=\"/dynamic_loading/1\"]").click();
+        cy.get("#start > button").click();
+
+        cy.get("#finish > h4")
+            .should("be.visible")
+            .should("have.text", "Hello World!");
+    });
+});
