cb0f2106958e9062ce6214e48f885e18f62f4985
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2020-10-10 16:19:01 +0300

    make all tests work

--- cypress/integration/the_internet_app/dynamic_loading_spec.js
+++ cypress/integration/the_internet_app/dynamic_loading_spec.js
@@ -6,7 +6,7 @@ describe("Dynamic loading page", function () {
     it('should display Hello World', function () {
         cy.get("[href=\"/dynamic_loading/1\"]").click();
         cy.get("#start > button").click();
-
+        cy.wait(5000)
         cy.get("#finish > h4")
             .should("be.visible")
             .should("have.text", "Hello World!");
